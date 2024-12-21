2d47ff30873ed56f6dec09b69e2ee51060ee6f8e
Muba <78711094+munkhbayarbr@users.noreply.github.com>
2024-12-21 15:56:11 +0800

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'; // Import Firebase Storage
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyADcc8KvuU-ng1bCaPCxOHKAdtRkk8xDqs',
-  authDomain: 'authimage-e7cbe.firebaseapp.com',
-  projectId: 'authimage-e7cbe',
-  storageBucket: 'authimage-e7cbe.appspot.com', // Corrected storage bucket
-  messagingSenderId: '1060966921647',
-  appId: '1:1060966921647:web:cc99f0a4fd7ed739d2eb5a',
-  measurementId: 'G-5PFFZEW2D2',
+ 
 };
 
 const app = initializeApp(firebaseConfig);
